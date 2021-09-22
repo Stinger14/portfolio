8d19e38d4b5485d28177a61b5c3abd092fa97933
Maxly Garcia <maxgarcia6@gmail.com>
2021-09-21 23:51:18 -0400

    Add tests for the pages app. Testing urls, templates, html.

--- pages/tests.py
+++ pages/tests.py
@@ -1,27 +1,21 @@
 from django.test import SimpleTestCase
-from django.urls import reverse, resolve
-
-from .views import HomePageView
+from django.urls import reverse
 
 
-class HomePageTest(SimpleTestCase):
+class HomepageTest(SimpleTestCase):
+
+    def setUp(self):
+        url = reverse('home')
+        self.res = self.client.get(url)
 
     def test_homepage_status_code(self):
-        res = self.client.get('/')
-        self.assertEqual(res.status_code, 200)
-
-    def test_homepage_url_name(self):
-        res = self.client.get(reverse('home'))
-        self.assertEqual(res.status_code, 200)
+        self.assertEqual(self.res.status_code, 200)
 
     def test_homepage_template(self):
-        res = self.client.get('/')
-        self.assertTemplateUsed(res, 'home.html')
+        self.assertTemplateUsed(self.res, 'home.html')
 
     def test_homepage_contains_correct_html(self):
-        res = self.client.get('/')
-        self.assertContains(res, 'Homepage')
+        self.assertContains(self.res, 'Homepage')
 
     def test_homepage_does_not_contain_incorrect_html(self):
-        res = self.client.get('/')
-        self.assertNotContains(res, 'hey, i should not be on the page!')
+        self.assertNotContains(self.res, 'hey, i should not be on the page!')
