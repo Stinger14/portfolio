6ed87603be497edb42fe7c9e1ea7ae08f6b80d2a
Maxly Garcia <maxgarcia6@gmail.com>
2021-10-01 11:47:18 -0400

    Create templates/account directory for third party user management framework(allauth). Update templates with allauth login, logout, signup allowing to delete users's custom management. Update project's settings and url link.

--- portfolio_project/urls.py
+++ portfolio_project/urls.py
@@ -21,9 +21,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     # User management
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('allauth.urls')),
+    # path('accounts/', include('django.contrib.auth.urls')),
 
     # Local apps
-    path('accounts/', include('users.urls')),
+    # path('accounts/', include('users.urls')),
     path('', include('pages.urls')),
 ]
