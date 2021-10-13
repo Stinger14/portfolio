93928510fb77311c8abe495d841b1a8f86aa0349
Maxly Garcia <maxgarcia6@gmail.com>
2021-10-13 08:47:51 -0400

    Add tests for the reviews app model, views and templates.

--- projects/tests.py
+++ projects/tests.py
@@ -1,19 +1,32 @@
+from django.contrib.auth import get_user_model
 from django.test import Client, TestCase
 from django.urls import reverse
 
-from .models import Project
+from .models import Project, Review
 
 
 class ProjectTests(TestCase):
     def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username='reviewuser',
+            email='reviewuser@email.com',
+            password='testpass123'
+        )
+
         self.project = Project.objects.create(
             title='Url Shortner',
-            description='More human readable link names.',
+            description='More human readable link names',
+        )
+
+        self.review = Review.objects.create(
+            project=self.project,
+            author=self.user,
+            review='An excellent review',
         )
 
     def test_project_listing(self):
         self.assertEqual(f'{self.project.title}', 'Url Shortner')
-        self.assertEqual(f'{self.project.description}', 'More human readable link names.')
+        self.assertEqual(f'{self.project.description}', 'More human readable link names')
 
     def test_project_list_view(self):
         response = self.client.get(reverse('project_list'))
@@ -27,4 +40,5 @@ class ProjectTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, 'Url Shortner')
+        self.assertContains(response, 'An excellent review')
         self.assertTemplateUsed(response, 'projects/project_detail.html')
